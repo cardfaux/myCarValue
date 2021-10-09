e1a1580a66d237a99147686a2039dcd98a8cddbc
cardfaux <cardfaux@tutanota.com>
2021-10-09 15:01:04 -0500

    added a custom user.dto and interceptor to exclude the password from the response

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+import { UserDto } from '../users/dtos/user.dto';
+
+export class SerializeInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    // Run something before a request is handled by the request handler
+    return handler.handle().pipe(
+      map((data: any) => {
+        // Run something before the response is sent out
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
